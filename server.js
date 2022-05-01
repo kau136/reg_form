03888d6faa692a6605495f21464d129e3acd26f8
kau136 <hkkaushik136@gmail.com>
2022-05-01 22:01:48 +0530

    tiny change

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http=require('http');
 const app=require('./app');
 const server=http.createServer(app);
+console.log('hemant')
 server.listen(3000,console.log("<--------SERVER IS RUNNING----------->"));
